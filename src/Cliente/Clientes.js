cb15d8f6432365202b917a0b37b407dfe1542b3b
KarenYanza <97999826+KarenYanza@users.noreply.github.com>
2023-03-14 21:37:12 -0500

    Cliente Cedula

--- src/Cliente/Clientes.js
+++ src/Cliente/Clientes.js
@@ -12,7 +12,7 @@ export default function Clientes() {
     const { id } = useParams();
     const [user, setUser] = useState({
         edad: null,
-        estado:true,
+        estado: true,
         persona: {
             nombre: null,
             apellido: null,
@@ -51,170 +51,179 @@ export default function Clientes() {
         event.preventDefault();
         const cedulaValida = /^\d{10}$/.test(user.persona.cedula);
         console.log(user)
-        if (user.persona.cedula != null && user.persona.direccion != null && user.persona.nombre != null && user.persona.apellido != null && user.persona.genero != null && user.edad != null ) {
+        if (user.persona.cedula != null && user.persona.direccion != null && user.persona.nombre != null && user.persona.apellido != null && user.persona.genero != null && user.edad != null) {
             if (cedulaValida == true) {
                 const result = await axios.post("http://localhost:8080/api/crearcliente", user);
-                console.log(result.data);
-                
-                navigate('/vListaCliente')
+                if (result.data != "") {
+                    console.log(result.data);
+
+
+                    navigate('/vListaCliente')
+                } else {
+                    setError("Cedula Duplicada")
+                    setTimeout(() => {
+                        setError("")
+                    }, 2000);
+                }
             } else {
+                console.log("error")
                 setError("Cedula incorrecta");
                 setTimeout(() => {
                     setError("")
                 }, 2000);
             }
 
-        }else {
+        } else {
             setError("Datos incompletos");
             setTimeout(() => {
-              setError("")
+                setError("")
             }, 2000);
-          }
-
-
-        }
-
-        function cancelar() {
-            navigate('/vListaCliente');
         }
 
 
-        const handleCedulaChange = (event) => {
-            const nuevaCedula = event.target.value;
-            setUser((prevState) => ({
-                ...prevState,
-                persona: {
-                    ...prevState.persona,
-                    cedula: nuevaCedula,
-                },
-            }));
-        };
+    }
 
-        const handlenombreChange = (event) => {
-            const nuevaCedula = event.target.value;
-            setUser((prevState) => ({
-                ...prevState,
-                persona: {
-                    ...prevState.persona,
-                    nombre: nuevaCedula,
-                },
-            }));
-        };
-
-        const handleapellidoChange = (event) => {
-            const nuevaCedula = event.target.value;
-            setUser((prevState) => ({
-                ...prevState,
-                persona: {
-                    ...prevState.persona,
-                    apellido: nuevaCedula,
-                },
-            }));
-        };
-
-        const handledireccionChange = (event) => {
-            const nuevaCedula = event.target.value;
-            setUser((prevState) => ({
-                ...prevState,
-                persona: {
-                    ...prevState.persona,
-                    direccion: nuevaCedula,
-                },
-            }));
-        };
-        const handlegeneroChange = (event) => {
-            const nuevaCedula = event.target.value;
-            setUser((prevState) => ({
-                ...prevState,
-                persona: {
-                    ...prevState.persona,
-                    genero: nuevaCedula,
-                },
-            }));
-        };
-        
-       
+    function cancelar() {
+        navigate('/vListaCliente');
+    }
 
 
-        return (
+    const handleCedulaChange = (event) => {
+        const nuevaCedula = event.target.value;
+        setUser((prevState) => ({
+            ...prevState,
+            persona: {
+                ...prevState.persona,
+                cedula: nuevaCedula,
+            },
+        }));
+    };
 
-            <div className="container" style={{ marginTop: "50px" }} >
-                {error && (
-                    <div className="alert alert-danger text-center" role="alert">
-                        {error}
-                    </div>
-                )}
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                        <h2 className="text-center m-4"> Registro de Cliente</h2>
-                        <form onSubmit={(e) => Registrar(e)}>
-                            <div>
-                                <br></br>
-                                Cedula:
-                                <input
-                                    placeholder="Ingrese su cedula"
-                                    value={user.persona.cedula}
-                                    onChange={handleCedulaChange}
-                                    style={{ marginLeft: "20px", marginRight: "5px", width: "160px" }}
-                                    type="text"
-                                    maxLength={10}
-                                    onInput={(e) => {
-                                        e.target.value = e.target.value.slice(0, 10)
-                                    }} />
-                                Dirección:
-                                <input
-                                    placeholder="Ingrese la direccion"
-                                    value={user.persona.direccion}
-                                    onChange={handledireccionChange}
-                                    style={{ marginLeft: "10px", marginTop: "3px", width: "160px" }} />
-                                <br />
-                                Nombre:
-                                <input
-                                    style={{ marginLeft: "10px", marginRight: "5px", marginTop: "3px", width: "160px" }}
-                                    placeholder="Ingrese el nombre"
-                                    value={user.persona.nombre}
-                                    onChange={handlenombreChange} />
-                                Apellido:
-                                <input
-                                    placeholder="Ingrese el apellido"
-                                    value={user.persona.apellido}
-                                    onChange={handleapellidoChange}
-                                    style={{ marginLeft: "18px", marginTop: "3px", width: "160px" }} />
-                                <br />
-                                Genero:
-                                <select
-                                    value={user.persona.genero}
-                                    onChange={handlegeneroChange}
-                                    style={{ marginLeft: "17px",marginRight: "5px", marginTop: "3px", width: "160px" }} >
-                                    <option value="">Seleccione</option>
-                                    <option value="MASCULINO">Masculino</option>
-                                    <option value="FEMENINO">Femenino</option>
-                                    <option value="OTRO">Otro</option>
-                                </select>
-                                Edad:
-                                <input
-                                    placeholder="Ingrese su edad"
-                                    type="number" 
-                                    value={user.edad}
-                                    onChange={(e) => setUser({ ...user, edad: e.target.value })}
-                                    maxLength={2}
-                                    onInput={(e) => {
-                                        e.target.value = e.target.value.slice(0, 2)
-                                    }} 
-                                    style={{ marginLeft: "12px", marginTop: "10px" }} />
-                                
-                                <br /><br />
-                                <button style={{ marginLeft: "100px" }} type="submit" className="btn btn-outline-primary" >GUARDAR</button>
-                                <button style={{ marginLeft: "20px" }} className="btn btn-outline-primary" onClick={cancelar}>CANCELAR</button>
+    const handlenombreChange = (event) => {
+        const nuevaCedula = event.target.value;
+        setUser((prevState) => ({
+            ...prevState,
+            persona: {
+                ...prevState.persona,
+                nombre: nuevaCedula,
+            },
+        }));
+    };
+
+    const handleapellidoChange = (event) => {
+        const nuevaCedula = event.target.value;
+        setUser((prevState) => ({
+            ...prevState,
+            persona: {
+                ...prevState.persona,
+                apellido: nuevaCedula,
+            },
+        }));
+    };
+
+    const handledireccionChange = (event) => {
+        const nuevaCedula = event.target.value;
+        setUser((prevState) => ({
+            ...prevState,
+            persona: {
+                ...prevState.persona,
+                direccion: nuevaCedula,
+            },
+        }));
+    };
+    const handlegeneroChange = (event) => {
+        const nuevaCedula = event.target.value;
+        setUser((prevState) => ({
+            ...prevState,
+            persona: {
+                ...prevState.persona,
+                genero: nuevaCedula,
+            },
+        }));
+    };
 
 
 
-                            </div>
-                        </form>
-                    </div>
 
+    return (
+
+        <div className="container" style={{ marginTop: "50px" }} >
+            {error && (
+                <div className="alert alert-danger text-center" role="alert">
+                    {error}
+                </div>
+            )}
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className="text-center m-4"> Registro de Cliente</h2>
+                    <form onSubmit={(e) => Registrar(e)}>
+                        <div>
+                            <br></br>
+                            Cedula:
+                            <input
+                                placeholder="Ingrese su cedula"
+                                value={user.persona.cedula}
+                                onChange={handleCedulaChange}
+                                style={{ marginLeft: "20px", marginRight: "5px", width: "160px" }}
+                                type="text"
+                                maxLength={10}
+                                onInput={(e) => {
+                                    e.target.value = e.target.value.slice(0, 10)
+                                }} />
+                            Dirección:
+                            <input
+                                placeholder="Ingrese la direccion"
+                                value={user.persona.direccion}
+                                onChange={handledireccionChange}
+                                style={{ marginLeft: "10px", marginTop: "3px", width: "160px" }} />
+                            <br />
+                            Nombre:
+                            <input
+                                style={{ marginLeft: "10px", marginRight: "5px", marginTop: "3px", width: "160px" }}
+                                placeholder="Ingrese el nombre"
+                                value={user.persona.nombre}
+                                onChange={handlenombreChange} />
+                            Apellido:
+                            <input
+                                placeholder="Ingrese el apellido"
+                                value={user.persona.apellido}
+                                onChange={handleapellidoChange}
+                                style={{ marginLeft: "18px", marginTop: "3px", width: "160px" }} />
+                            <br />
+                            Genero:
+                            <select
+                                value={user.persona.genero}
+                                onChange={handlegeneroChange}
+                                style={{ marginLeft: "17px", marginRight: "5px", marginTop: "3px", width: "160px" }} >
+                                <option value="">Seleccione</option>
+                                <option value="MASCULINO">Masculino</option>
+                                <option value="FEMENINO">Femenino</option>
+                                <option value="OTRO">Otro</option>
+                            </select>
+                            Edad:
+                            <input
+                                placeholder="Ingrese su edad"
+                                type="number"
+                                value={user.edad}
+                                onChange={(e) => setUser({ ...user, edad: e.target.value })}
+                                maxLength={2}
+                                onInput={(e) => {
+                                    e.target.value = e.target.value.slice(0, 2)
+                                }}
+                                style={{ marginLeft: "12px", marginTop: "10px" }} />
+
+                            <br /><br />
+                            <button style={{ marginLeft: "100px" }} type="submit" className="btn btn-outline-primary" >GUARDAR</button>
+                            <button style={{ marginLeft: "20px" }} className="btn btn-outline-primary" onClick={cancelar}>CANCELAR</button>
+
+
+
+                        </div>
+                    </form>
                 </div>
 
             </div>
-        )
-    }
+
+        </div>
+    )
+}
